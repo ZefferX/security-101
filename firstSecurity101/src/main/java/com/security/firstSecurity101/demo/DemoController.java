e807c84824014690d8372c5d5ed650e7e3156536
ZefferX <christofer2497@gmail.com>
2024-07-27 00:46:28 -0300

    database from create-drop to update @EnableMethodSecurity added HasAuthority tested for distinct roles

--- firstSecurity101/src/main/java/com/security/firstSecurity101/demo/DemoController.java
+++ firstSecurity101/src/main/java/com/security/firstSecurity101/demo/DemoController.java
@@ -1,14 +1,20 @@
 package com.security.firstSecurity101.demo;
 
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 @RequestMapping("/api/v1/demo-controller")
 public class DemoController {
 
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from secured endpoint");
